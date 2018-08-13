2f0ab7a870447641546c2297c90190333cfe69e2
ornellaharinjaka <andrianasolonella@gmail.com>
2018-08-13 13:34:09 +0300

    second commit

---
+++ main.js
@@ -0,0 +1,32 @@
+var number = document.getElementsByClassName('buttons');
+var input = document.getElementById('input');
+
+//add event to number
+for (var i = 0; i < number.length ; i++){
+    number[i].addEventListener('click', addNumber);
+}
+
+//function addNumber
+function addNumber(e){
+
+    if(e.target.dataset.value == "C")
+    {
+        input.innerHTML  = "";
+
+    }else if(e.target.dataset.value == "=")
+    {
+        input.innerHTML = eval(input.innerHTML);
+
+    }
+    else{
+        input.innerHTML = input.innerHTML.concat((e.target.dataset.value));
+    }
+    
+}
+
+//function boolen
+/*function isInfinite(eval(input.innerHTML)){
+    return value === value / 0;
+}*/
+
+/*.inerhtml = isInfinite(eval(.......)) ? 'divbyzero' : eval(...);*/
